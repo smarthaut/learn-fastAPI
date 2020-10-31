d67b73d03808bef01e0c3244a0751a8f0a07443f
yibiqinnian <943298665@qq.com>
2020-10-31 09:41:54 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from enum import Enum
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
+
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name}
+    if model_name == ModelName.lenet:
+        return {"model_name": model_name}
+    return {"model_name":model_name}
